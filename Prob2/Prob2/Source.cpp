3a7d48c57f929e22a18fda3b44fcbd1627ef8c78
Crisstina28 <91867423+Crisstina28@users.noreply.github.com>
2021-10-26 22:30:08 +0300

    Update Source.cpp

--- Prob2/Prob2/Source.cpp
+++ Prob2/Prob2/Source.cpp
@@ -2,17 +2,30 @@
 #include <cmath>
 
 using namespace std;
+void citire(int &x)
+{
+    cin >> x;
+  
+}
+
+void  prelucrare(int a, int b, int c)
+{
+    int perimetru, arie;
+    if ((a * b * c != 0) && (a + b > c) && (b + c > a) && (a + c > b)) {
+        perimetru = a + b + c;
+        cout << "Perimetrul: " << perimetru << endl;
+        arie = perimetru / 2;
+        cout << "Aria: " << sqrt(arie * (arie - a) * (arie - b) * (arie - c));
+    }
+}
 
 int main() {
 
-    int a, b, c, P, p;
-    cin >> a >> b >> c;
+    int a, b,c;
+    citire(a);
+    citire(b);
+    citire(c);
 
-    if ((a * b * c != 0) && (a + b > c) && (b + c > a) && (a + c > b)) {
-        P = a + b + c;
-        cout << "Perimetrul: " << P << endl;
-        p = P / 2;
-        cout << "Aria: " << sqrt(p * (p - a) * (p - b) * (p - c));
-    }
+    prelucrare(a, b, c);
     return 0;
 }
