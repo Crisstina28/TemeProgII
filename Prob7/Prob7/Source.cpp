0520f65966a46d5187a581cadda9a08f64a112b9
Crisstina28 <91867423+Crisstina28@users.noreply.github.com>
2021-10-26 22:28:33 +0300

    Update Source.cpp

--- Prob7/Prob7/Source.cpp
+++ Prob7/Prob7/Source.cpp
@@ -4,24 +4,26 @@
 using namespace std;
 
 ifstream fin("in.txt");
-ofstream fout("out.txt");
+ofstream fout("ou.txt");
 
 struct elev {
     int nr;
     int pp;
 };
 
-int main() {
-    int a[100][100];
-    int n, k = 0, ppl;
-    bool ok;
+void citire(int a[100][100], int n)
+{
     fin >> n;
-    elev e[100];
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
             fin >> a[i][j];
     }
 
+}
+
+void eleviiCareSeAgreazaRecipropc(int a[100][100], int n)
+{
     fout << "Elevii care se agreaza reciproc: " << endl;
 
     for (int i = 1; i < n; i++) {
@@ -30,7 +32,11 @@ int main() {
                 fout << i + 1 << " " << j + 1 << endl;
         }
     }
+}
 
+void eleviCareNuSuntAgreatiDeNimeni(int a[100][100], int n)
+{
+    bool ok;
     fout << "Elevii care nu sunt agreati de nimeni: " << endl;
 
     for (int i = 0; i < n; i++) {
@@ -43,6 +49,11 @@ int main() {
             fout << i + 1 << endl;
     }
 
+}
+
+void eleviCareNuAgreazaPeNimeni(int a[100][100], int n)
+{
+    bool ok;
     fout << "Elevii care nu agreaza pe nimeni: " << endl;
 
     for (int i = 0; i < n; i++) {
@@ -54,7 +65,12 @@ int main() {
         if (ok == 0)
             fout << i + 1 << endl;
     }
+}
 
+void eleviInOrdineaPopularitaii(int a[100][100], int n)
+{
+    int  k = 0, ppl;
+    elev e[100];
     fout << "Elevii in ordinea popularitatii: " << endl;
 
     for (int i = 0; i < n; i++) {
@@ -76,7 +92,16 @@ int main() {
                 fout << e[i].nr + 1 << endl;
         }
     } while (ppl != 0);
+}
 
-
+int main() {
+    int a[100][100], n;
+    citire(a, n);
+    eleviiCareSeAgreazaRecipropc(a, n);
+    eleviCareNuSuntAgreatiDeNimeni(a, n);
+    eleviCareNuAgreazaPeNimeni(a, n);
+    eleviInOrdineaPopularitaii(a, n);
     return 0;
 }
+
+   
