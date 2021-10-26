5c39334af775d66cfa34131eab7278c79b292c24
Crisstina28 <91867423+Crisstina28@users.noreply.github.com>
2021-10-26 22:31:04 +0300

    Update Source.cpp

--- Prob3/Prob3/Source.cpp
+++ Prob3/Prob3/Source.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+void citire(int v[], int &n, int &x)
+{
+    cin >> n;
+    for (int k = 0; k < n; k++) {
+        cin >> v[k];
+    }
+    cin >> x;
+
+}
+
 int nrap2(int a[], int n, int x, int i, int& ap) {
     if (n == i) {
         if (a[i] == x)
@@ -21,10 +31,8 @@ int nrap2(int a[], int n, int x, int i, int& ap) {
 
 int main() {
     int v[100], n, x, apar = 0, i = 0;
-    cin >> n >> x;
-    for (int k = 0; k < n; k++) {
-        cin >> v[k];
-    }
+
+    citire(v, n, x);
     cout << nrap2(v, n, x, i, apar);
 
     return 0;
